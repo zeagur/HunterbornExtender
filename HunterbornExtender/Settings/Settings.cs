648b8d51b445e6c321ca38ecf106c3a70f69956a
MarkDF <markdfairchild@gmail.com>
2022-11-27 12:51:59 -0800

    Simple Settings file to use for serialization and deserialization (even if it doesn't end up being populated by SetAutogeneratedSettings in the pipeline).

--- HunterbornExtender/Settings/Settings.cs
+++ HunterbornExtender/Settings/Settings.cs
@@ -1,5 +1,14 @@
 ﻿namespace HunterbornExtender.Settings;
 
+using Mutagen.Bethesda.Synthesis.States.DI;
+using HunterbornExtenderUI;
+
+
 public class Settings
 {
+    public Plugin[] Plugins { get; set; } = new Plugin[0];
+
+    public DeathItemSelection[] DeathItemSelections { get; set; } = new DeathItemSelection[0];
+
+
 }
