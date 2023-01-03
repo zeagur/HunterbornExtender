9f3399c74bb3543bc0cfd22098621c1785eb4143
Mark <markdfairchild@gmail.com>
2023-01-03 04:35:05 -0800

    Fixed debugging line.

--- HunterbornExtender/LegacyConverter.cs
+++ HunterbornExtender/LegacyConverter.cs
@@ -40,7 +40,7 @@ namespace HunterbornExtender
 
                     if (filenames.Count > 0) Write.Success(1, $"Found {filenames.Count} json files.");
                     else Write.Fail(1, $"No json files found.");
-                    print(previousFilenames);
+                    Write.Action(0, $"Previous filenames: {previousFilenames}");
 
                     foreach (var filename in filenames)
                     {
